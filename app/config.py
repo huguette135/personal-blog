70244e12ddd63bdc7aa41e85a7e87b4cb2615baa
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-12-08 12:24:43 +0200

    updating changes

--- app/config.py
+++ app/config.py
@@ -38,6 +38,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://huguet:135@localhost/blog2'
 
     DEBUG = True
     
---
+++ start.sh
@@ -0,0 +1,4 @@
+export SECRET_KEY=Flask
+export MAIL_USERNAME=hyguetteumutoni@gmail.com
+export MAIL_PASSWORD=tonton135
+python3.6 manage.py server
